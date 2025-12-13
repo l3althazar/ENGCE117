ccdd3f8ba5797af080f12c3c78de2402fb24ddca
สัญญา ลุงจาย <pomking05@gmail.com>
2025-12-13 12:45:19 +0700

    lab1.2

--- Week1/lab1.2.c
+++ Week1/lab1.2.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void GetSet(int data[], int *num);
+void GetSet(int **, int *);
 
 int main() {
-    int data[100];
-    int num;
-
-    // Fill array and get the number of elements
-    GetSet(data, &num);
-
-    // Print all elements
-    for (int i = 0; i < num; i++) {
-        printf("[%d]: %d\n", i, data[i]);
-    }
-    
+    int *data, num;
+    GetSet(&data, &num);
+    free(data);
     return 0;
 }
 
-void GetSet(int data[], int *num) {
-    // Read number of elements
-    scanf("%d", num);
-
-    // Read each element
-    for (int i = 0; i < *num; i++) {
-        scanf("%d", &data[i]);
+void GetSet(int **set, int *n) {
+    scanf("%d", n);
+    *set = (int*)malloc(*n * sizeof(int));
+    for (int i = 0; i < *n; i++) {
+        scanf("%d", &(*set)[i]);
+    }
+    printf("%d\n", *n);
+    for (int i = 0; i < *n; i++) {
+        printf("%d\n", i, (*set)[i]);
     }
 }
