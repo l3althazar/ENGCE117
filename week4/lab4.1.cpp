7436afb7c095ab5e278c2fba7c1f55e72f4d5568
สัญญา ลุงจาย <pomking05@gmail.com>
2025-12-22 18:00:08 +0700

    4.1 V1

---
+++ week4/lab4.1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void go(int **p, int *z);
+
+int main() {
+    int *a, b = 10, c = 20;
+    go(&a, &b);
+    printf("%d %p %p\n", *a, (void*)a, (void*)&a);
+    go(&a, &c);
+    printf("%d %p %p\n", *a, (void*)a, (void*)&a);
+    return 0;
+}
+
+void go(int **p, int *z) {
+    *p = z;
+}
