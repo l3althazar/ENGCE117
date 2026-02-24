76a87d8db9843cb75bd8beb70bec7554e51a54a7
สัญญา ลุงจาย <pomking05@gmail.com>
2026-02-24 22:19:28 +0700

    12.2 update

--- week12/12.2.cpp
+++ week12/12.2.cpp
@@ -1,87 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int *KnapsackGreedy( int *w, int *v, int n, int wx );
+int *KnapsackGreedy( int *w, int *v, int n, int wx )
+{
+    int *selected = new int[ n ];
+    int index, j;
+
+    for ( index = 0; index < n; index++ )
+        selected[ index ] = 0;
+
+    for ( index = 0; index < n - 1; index++ )
+    {
+        for ( j = index + 1; j < n; j++ )
+        {
+            double r1 = (double)v[ index ] / w[ index ];
+            double r2 = (double)v[ j ] / w[ j ];
+
+            if ( r2 > r1 )
+            {
+                int temp;
+
+                temp = w[ index ];
+                w[ index ] = w[ j ];
+                w[ j ] = temp;
+
+                temp = v[ index ];
+                v[ index ] = v[ j ];
+                v[ j ] = temp;
+            }
+        }
+    }
+
+    int currentWeight = 0;
+
+    for ( index = 0; index < n; index++ )
+    {
+        if ( currentWeight + w[ index ] <= wx )
+        {
+            selected[ index ] = 1;
+            currentWeight += w[ index ];
+        }
+    }
+
+    return selected;
+}
 
 int main()
 {
-    int n = 5;
-    int wx = 11;
-
+    int n = 5, wx = 11;
     int w[ 5 ] = { 1, 2, 5, 6, 7 };
     int v[ 5 ] = { 1, 6, 18, 22, 28 };
 
     int *x = KnapsackGreedy( w, v, n, wx );
 
-    int index;
-
-    for ( index = 0; index < n; index++ )
-    {
-        printf( "%d ", x[ index ] );
-    }
-
-    free( x );
+    for ( int i = 0; i < n; i++ )
+        printf( "%d ", x[ i ] );
 
     return 0;
-}
-
-int *KnapsackGreedy( int *w, int *v, int n, int wx )
-{
-    int *selectedItems;
-    int *used;
-    int itemIndex;
-    int bestIndex;
-    int remainingWeight;
-    double bestRatio;
-    double currentRatio;
-
-    selectedItems = (int *)malloc( n * sizeof( int ) );
-    used = (int *)malloc( n * sizeof( int ) );
-
-    for ( itemIndex = 0; itemIndex < n; itemIndex++ )
-    {
-        selectedItems[ itemIndex ] = 0;
-        used[ itemIndex ] = 0;
-    }
-
-    remainingWeight = wx;
-
-    while ( 1 )
-    {
-        bestRatio = -1.0;
-        bestIndex = -1;
-
-        for ( itemIndex = 0; itemIndex < n; itemIndex++ )
-        {
-            if ( used[ itemIndex ] == 0 )
-            {
-                if ( w[ itemIndex ] <= remainingWeight )
-                {
-                    currentRatio =
-                        (double)v[ itemIndex ] /
-                        (double)w[ itemIndex ];
-
-                    if ( currentRatio > bestRatio )
-                    {
-                        bestRatio = currentRatio;
-                        bestIndex = itemIndex;
-                    }
-                }
-            }
-        }
-
-        if ( bestIndex == -1 )
-        {
-            break;
-        }
-
-        selectedItems[ bestIndex ] = 1;
-        used[ bestIndex ] = 1;
-        remainingWeight =
-            remainingWeight - w[ bestIndex ];
-    }
-
-    free( used );
-
-    return selectedItems;
 }
