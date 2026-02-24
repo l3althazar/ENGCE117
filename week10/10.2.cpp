ad213b06c05cbdeccf32eed532a2cf26ea3f5b22
สัญญา ลุงจาย <pomking05@gmail.com>
2026-02-24 21:39:49 +0700

    10.2 update

--- week10/10.2.cpp
+++ week10/10.2.cpp
@@ -2,25 +2,29 @@
 
 void TowerHanoi( int m, int i, int j );
 
-int main() {
-
+int main()
+{
     TowerHanoi( 3, 1, 3 );
-
     return 0;
 }
 
-void TowerHanoi( int m, int i, int j ) {
-
+void TowerHanoi( int m, int i, int j )
+{
     int k;
 
-    if ( m == 1 ) {
-        printf( "Disc 1 from %d to %d\n", i, j );
-    }
-    else {
-        k = 6 - i - j;
 
-        TowerHanoi( m - 1, i, k );
-        printf( "Disc %d from %d to %d\n", m, i, j );
-        TowerHanoi( m - 1, k, j );
+    if ( m == 1 )
+    {
+        printf( "Disc 1 from %d to %d\n", i, j );
+        return;
     }
+
+
+    k = 6 - i - j;
+
+    TowerHanoi( m - 1, i, k );
+
+    printf( "Disc %d from %d to %d\n", m, i, j );
+
+    TowerHanoi( m - 1, k, j );
 }
